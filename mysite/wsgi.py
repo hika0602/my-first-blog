ed4b5111dad1d3db5b107d0cf2c7f192146bf8c2
hika0602 <hikarin0602@icloud.com>
2021-11-05 15:14:42 +0100

    Modified templates to display posts from database.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path ='/d/耀/python/djangogirls'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
