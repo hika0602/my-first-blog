423c63c7c32289b6227b0beb36ab923332789806
hika0602 <hikarin0602@icloud.com>
2021-11-06 02:49:47 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from django.views.generic import ListView
  
 class PostListView(ListView):
     model = Post
- 
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
